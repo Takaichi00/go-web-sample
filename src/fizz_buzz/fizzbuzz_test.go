a7848d81730a6efcb12d6878635cc110df5af000
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-10-06 21:50:18 +0900

    feat: impl table test

--- src/fizz_buzz/fizzbuzz_test.go
+++ src/fizz_buzz/fizzbuzz_test.go
@@ -6,6 +6,24 @@ import (
 )
 
 func TestFizzBuzz(t *testing.T) {
+
+	tests := []struct {
+		n    int
+		want string
+	}{
+		{n: 1, want: "1"},
+		{n: 2, want: "2"},
+		{n: 3, want: "Fizz"},
+		{n: 5, want: "Buzz"},
+	}
+
+	for _, tt := range tests {
+		got := fizzbuzz.Convert(tt.n)
+		if got != tt.want {
+			t.Errorf(`FizzBuzz(1) is %q`, got)
+		}
+	}
+
 	got := fizzbuzz.Convert(1)
 	if got != "1" {
 		t.Errorf(`FizzBuzz(1) is %q`, got)
