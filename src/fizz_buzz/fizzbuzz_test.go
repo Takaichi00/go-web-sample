f18c30adc62899553f55b43f4edec1fcfc58d25c
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-10-07 13:10:44 +0900

    refactor: parallel execute

--- src/fizz_buzz/fizzbuzz_test.go
+++ src/fizz_buzz/fizzbuzz_test.go
@@ -1,6 +1,7 @@
 package fizzbuzz_test
 
 import (
+	"fmt"
 	"testing"
 	fizzbuzz "todo/src/fizz_buzz"
 )
@@ -19,10 +20,22 @@ func TestFizzBuzz(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		got := fizzbuzz.Convert(tt.n)
-		if got != tt.want {
-			t.Errorf(`FizzBuzz(1) is %q`, got)
-		}
+		tt := tt                               // prepare local val (for parallel)
+		name := fmt.Sprintf("number:%v", tt.n) // test name
+
+		// execute sub test
+		t.Run(name, func(t *testing.T) {
+			t.Parallel() // execute parallel
+			got := fizzbuzz.Convert(tt.n)
+			if got != tt.want {
+				t.Errorf(`Convert(%v) = %q but want %q`, tt.n, got, tt.want)
+			}
+		})
+
+		//got := fizzbuzz.Convert(tt.n)
+		//if got != tt.want {
+		//	t.Errorf(`FizzBuzz(1) is %q`, got)
+		//}
 	}
 
 	//got := fizzbuzz.Convert(1)
