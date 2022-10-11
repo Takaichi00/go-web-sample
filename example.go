8bb622711bad21863edc091c17e3c816b3955222
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-10-12 01:29:45 +0900

    feat: impl fizzbuzz endpoint

--- example.go
+++ example.go
@@ -1,17 +1,29 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
+	"strconv"
+	fizzbuzz "todo/src/fizz_buzz"
 
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-  r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/fizzbuzz/:num", func(c *gin.Context) {
+		num, _ := strconv.Atoi(c.Param("num"))
+		result := fizzbuzz.Convert(num)
+		c.JSON(http.StatusOK, gin.H{
+			"result": result,
+		})
+
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
