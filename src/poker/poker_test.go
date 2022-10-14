7d58c34744de8da77119325e7c3b9d23b0e5ed17
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-10-14 19:37:05 +0900

    refactor: parallel testing for Card

--- src/poker/poker_test.go
+++ src/poker/poker_test.go
@@ -1,12 +1,31 @@
 package poker
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestCard(t *testing.T) {
-	card := Card{Suit: "♠", Rank: "3"}
-	if card.Notation() != "3♠" {
-		t.Errorf(`Card(1) is %q`, card)
+
+	tests := []struct {
+		suit string
+		rank string
+		want string
+	}{
+		{suit: "♠", rank: "3", want: "3♠"},
 	}
+
+	for _, tt := range tests {
+		tt := tt
+		name := fmt.Sprintf("want:%v", tt.want)
+
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			card := Card{Suit: tt.suit, Rank: tt.rank}
+			if card.Notation() != "3♠" {
+				t.Errorf(`Card(1) is %q`, card)
+			}
+		})
+	}
+
 }
