436e910c4faa7c74ff0301f7c087416b46a39b93
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2023-09-25 17:30:54 +0900

    feat: :+1: Straight のパターンを追加

--- src/poker/poker.go
+++ src/poker/poker.go
@@ -67,8 +67,9 @@ func (p *Cards) hand() Hand {
 	if p.Cards[0].hasSameSuit(p.Cards[1]) {
 		return Flush
 	}
-	if math.Abs(float64(p.Cards[0].rank.number-p.Cards[1].rank.number)) == 1 {
+	if math.Abs(float64(p.Cards[0].rank.number-p.Cards[1].rank.number)) == 1 || math.Abs(float64(p.Cards[0].rank.number-p.Cards[1].rank.number)) == 12 {
 		return Straight
 	}
+
 	return HighCard
 }
